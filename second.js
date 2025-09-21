3560f6f554fb02381f646f840ceeb570bbe87d53
hamza <ah9132517@gmail.com>
2025-09-21 23:15:15 +0500

    color_changer

---
+++ second.js
@@ -0,0 +1,22 @@
+const green = document.querySelector('#green')
+const yellow = document.querySelector('#yellow')
+const grey = document.querySelector('#grey')
+const purple = document.querySelector('#purple')
+
+green.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'green'
+})
+yellow.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'yellow'
+})
+grey.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'grey'
+})
+purple.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'purple'
+})
+
+
+
+
+
